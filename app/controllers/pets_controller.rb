8feca106caf6d6e220c034a6d977363f152ce47a
Artur <info@aigroupusa.com>
2021-05-01 16:38:54 -0400

    commit tree e33c25d1d5e5145998cf270c09e75d5a4fd1a89b

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,20 +6,49 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    #binding.pry
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    #binding.pry
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(params[:owner])
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do 
+    #binding.pry
+    @owners = Owner.all
     @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do 
+    #binding.pry
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    #binding.pry
+    #raise @pet.owner.name.inspect
+
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
+    #binding.pry
+    # if !params[:owner].keys.include?("pet_ids")
+    #   params[:owner]["pet_ids"] = []
+    #   end
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(params[:owner])
+      @owner.pets << @pet
+    end
 
     redirect to "pets/#{@pet.id}"
   end
